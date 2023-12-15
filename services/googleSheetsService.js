31cb08585b74c56f5600dd0d5dcae3f35fea3474
kirstenval03 <kirs0307@gmail.com>
2023-12-15 05:08:39 -0600

    Manage duplicate Registrants data

--- services/googleSheetsService.js
+++ services/googleSheetsService.js
@@ -2,11 +2,10 @@ const { google } = require('googleapis');
 const Customer = require('../models/Customer'); 
 
 const sheets = google.sheets('v4');
-
 const API_KEY = process.env.GOOGLE_SHEETS_API_KEY; 
 
-const spreadsheetId = '1Guueh-T_u6FaaANjoAMbY_N9vfGxdYt9oNd_F6aqhpg'; // Replace with your Spreadsheet ID
-const range = 'Registrations!B2:K'; 
+const spreadsheetId = '1Guueh-T_u6FaaANjoAMbY_N9vfGxdYt9oNd_F6aqhpg';
+const range = 'Registrations!B2:K';
 
 const columnMapping = {
     'FIRST NAME': 'firstName',
@@ -28,30 +27,36 @@ async function fetchAndSaveCustomerData() {
         });
 
         const rows = response.data.values;
-        if (rows.length > 1) { // Checking if there's more than just the header row
-            // Process each row and save to MongoDB
-            rows.slice(1).forEach(async (row) => { // Skip header row
+        if (rows.length > 1) { 
+            for (const row of rows.slice(1)) { 
                 let newCustomerData = {};
 
                 row.forEach((cell, index) => {
-                    const columnName = response.data.values[0][index]; // Column name from header row
+                    const columnName = rows[0][index]; 
                     const schemaFieldName = columnMapping[columnName];
                     if (schemaFieldName) {
                         if (schemaFieldName === 'vip') {
-                            newCustomerData[schemaFieldName] = cell.toUpperCase() === 'TRUE'; // Convert string to Boolean
+                            newCustomerData[schemaFieldName] = cell.toUpperCase() === 'TRUE';
                         } else if (schemaFieldName === 'revenue') {
-                            newCustomerData[schemaFieldName] = parseFloat(cell) || 0; // Convert string to number
+                            newCustomerData[schemaFieldName] = parseFloat(cell) || 0;
                         } else if (schemaFieldName === 'date') {
-                            newCustomerData[schemaFieldName] = new Date(cell); // Convert string to Date
+                            newCustomerData[schemaFieldName] = new Date(cell);
                         } else {
                             newCustomerData[schemaFieldName] = cell;
                         }
                     }
                 });
 
-                const newCustomer = new Customer(newCustomerData);
-                await newCustomer.save();
-            });
+                // Check if customer already exists
+                const existingCustomer = await Customer.findOne({ email: newCustomerData.email });
+                if (!existingCustomer) {
+                    const newCustomer = new Customer(newCustomerData);
+                    await newCustomer.save();
+                    console.log(`Imported new customer: ${newCustomerData.email}`);
+                } else {
+                    console.log(`Customer with email ${newCustomerData.email} already exists. Skipping.`);
+                }
+            }
 
             console.log('Customers imported successfully.');
         } else {
