1c26b51b09dced7a0ebcd76ed753c84a83380945
kirstenval03 <kirs0307@gmail.com>
2023-12-14 22:57:34 -0600

    Sidebar name working

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,29 @@
 var express = require('express');
 var router = express.Router();
 const User = require("../models/User");
+const isAuthenticated = require('../middleware/isAuthenticated');
 
-/* GET user by email. */
-router.get('/:email', async (req, res) => {
+/* GET current authenticated user. */
+router.get('/current', isAuthenticated, async (req, res) => {
   try {
-    const userEmail = req.params.email;
-    const user = await User.findOne({ email: userEmail });
+    // The isAuthenticated middleware should add the user to req
+    const userId = req.user._id; // Assuming your middleware adds user ID to req.user
+    const user = await User.findById(userId);
 
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
 
-    // Return the user data as JSON
-    res.json(user);
+    // Set headers to prevent caching
+    res.set('Cache-Control', 'no-store');
+    res.set('Pragma', 'no-cache');
+    res.set('Expires', 0);
+
+    // Return the user data as JSON, excluding sensitive fields
+    const { firstName, lastName, role } = user.toObject();
+    res.json({ firstName, lastName, role }); // Ensure this is an object
   } catch (error) {
-    console.error('Error fetching user data:', error);
+    console.error('Error fetching current user data:', error);
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
