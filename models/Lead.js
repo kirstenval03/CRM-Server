923529884273510467d6e01407cd3c29d890e448
kirstenval03 <kirs0307@gmail.com>
2023-12-17 23:44:23 -0600

    Color Change for CRM working

--- models/Lead.js
+++ models/Lead.js
@@ -1,13 +1,17 @@
 const { Schema, model } = require('mongoose');
 
 const leadSchema = new Schema({
-    name:String,
-    email:String,
-    coachName:String,
-    coachEmail:String, 
-},{
+    name: String,
+    email: String,
+    coachName: String,
+    coachEmail: String, 
+    statusColor: {
+        type: String,
+        enum: ['yellow', 'green', 'red', ''], 
+        default: ''
+    }
+}, {
     timestamps: true
-}
-)
+});
 
-module.exports = model('Lead', leadSchema)
+module.exports = model('Lead', leadSchema);
--- routes/lead.js
+++ routes/lead.js
@@ -45,4 +45,23 @@ router.get('/:leadId', isAuthenticated, (req, res, next) => {
         });
 });
 
+// UPDATE LEAD STATUS COLOR
+router.patch('/:leadId/update-color', async (req, res) => {
+    const { leadId } = req.params;
+    const { statusColor } = req.body; // Change this line
+
+    console.log(`Updating color for lead ${leadId} to ${statusColor}`); // Update this line
+
+    try {
+        const updatedLead = await Lead.findByIdAndUpdate(leadId, { statusColor }, { new: true });
+        if (!updatedLead) {
+            return res.status(404).send('Lead not found.');
+        }
+        res.json(updatedLead);
+    } catch (error) {
+        console.error('Error updating lead color:', error);
+        res.status(400).send('Invalid request data.');
+    }
+});
+
 module.exports = router;
