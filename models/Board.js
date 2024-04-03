d3a46fbaa6cd4ea0f35fa9bb061689f6ec2254bc
kirstenval03 <kirs0307@gmail.com>
2024-04-03 09:58:04 -0600

    Board view with pipeline status

--- models/Board.js
+++ models/Board.js
@@ -9,7 +9,7 @@ const cardSchema = new Schema({
 // Define Column schema with a reference to Contact
 const columnSchema = new Schema({
     title: String,
-    indexPosition: Number,
+    position: Number,
     cards: [cardSchema],
 });
 
@@ -29,3 +29,39 @@ module.exports = {
     Column,
     Card,
 };
+
+// const { Schema, model } = require('mongoose');
+
+// //COLUMNS
+// const columnSchema = new Schema({
+//     board: { type: Schema.Types.ObjectId, ref: 'Board' },
+//     title: String,
+//     position: Number,
+// },{ timestamps: true });
+
+// //CONTACTS / CARDS
+// const cardSchema = new Schema({
+//     eventId: { type: Schema.Types.ObjectId, ref: 'Event' }, 
+//     contactId: { type: Schema.Types.ObjectId, ref: 'Event.contacts' },
+//     position: Number, 
+//     column: { type: Schema.Types.ObjectId, ref: 'Column' }
+// },{ timestamps: true });
+
+
+// // BOARD
+// const boardSchema = new Schema({
+//     eventId: { type: Schema.Types.ObjectId, ref: 'Event' }, 
+//     columns: [columnSchema], 
+
+// });
+
+
+// const Board = model('Board', boardSchema);
+// const Column = model('Column', columnSchema);
+// const Card = model('Card', cardSchema);
+
+// module.exports = {
+//     Board,
+//     Column,
+//     Card,
+// };
